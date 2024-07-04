fc31cbf7e4b6e7f65ee7305eade9b0e4b16ee2d5
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-07-04 23:06:31 +0530

    all in one menu

--- menusdemo/app/src/main/java/com/example/menusdemo/MainActivity.java
+++ menusdemo/app/src/main/java/com/example/menusdemo/MainActivity.java
@@ -1,58 +1,81 @@
 package com.example.menusdemo;
 
+
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
-import androidx.appcompat.widget.Toolbar;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    RelativeLayout relativeLayout;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        textView = findViewById(R.id.textView);
+        relativeLayout = findViewById(R.id.relLayout);
+        button = findViewById(R.id.clickBtn);
+        button.setOnClickListener(view -> {
+            // Initializing the popup menu and giving the reference as current context
+            PopupMenu popupMenu = new PopupMenu(MainActivity.this, button);
+
+            // Inflating popup menu from popup_menu.xml file
+            popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(menuItem -> {
+                // Toast message on menu item clicked
+                Toast.makeText(MainActivity.this, "You Clicked " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            });
+            // Showing the popup menu
+            popupMenu.show();
         });
+        registerForContextMenu(textView);
+
     }
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        MenuInflater menuInflater= new MenuInflater(this);
+        menuInflater.inflate(R.menu.option_menu,menu);
         return true;
     }
-
-    public boolean onOptionsItemSelected (MenuItem item) {
-        if(item.getItemId() == R.id.item1)
-            Toast.makeText(MainActivity.this,"Create a New Project", Toast.LENGTH_SHORT).show();
-        if(item.getItemId() == R.id.item2)
-            Toast.makeText(MainActivity.this,"Open a Project", Toast.LENGTH_SHORT).show();
-        if(item.getItemId() == R.id.item3)
-            Toast.makeText(MainActivity.this,"Help", Toast.LENGTH_SHORT).show();
-        if(item.getItemId() == R.id.gi1)
-            Toast.makeText(MainActivity.this,"Group item 1", Toast.LENGTH_SHORT).show();
-        if(item.getItemId() == R.id.gi2)
-            Toast.makeText(MainActivity.this,"Group Item 2", Toast.LENGTH_SHORT).show();
-        if(item.getItemId() == R.id.s1)
-            Toast.makeText(MainActivity.this,"Sub Item 1", Toast.LENGTH_SHORT).show();
-        if(item.getItemId() == R.id.s2)
-            Toast.makeText(MainActivity.this,"Sub Item 2", Toast.LENGTH_SHORT).show();
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        // you can set menu header with title icon etc
+        menu.setHeaderTitle("Choose a color");
+        // add menu items
+        menu.add(0, v.getId(), 0, "Yellow");
+        menu.add(0, v.getId(), 0, "Gray");
+        menu.add(0, v.getId(), 0, "Cyan");
     }
 
-
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getTitle().equals("Yellow")) {
+            relativeLayout.setBackgroundColor(Color.YELLOW);
+        } else if (item.getTitle().equals("Gray")) {
+            relativeLayout.setBackgroundColor(Color.GRAY);
+        } else if (item.getTitle().equals("Cyan")) {
+            relativeLayout.setBackgroundColor(Color.CYAN);
+        }
+        return true;
+    }
 }
